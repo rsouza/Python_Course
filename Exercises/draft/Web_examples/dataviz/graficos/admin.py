ef16af990c494522aa995e8dddd0ed5886a49191
Renato Rocha Souza <rsouza.fgv@gmail.com>
2020-08-01 18:33:35 +0200

    moving

--- Exercises/draft/Web_examples/dataviz/graficos/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Grafico
-
-# Register your models here.
-admin.site.register(Grafico)
--- Exercises/draft/Web_examples/dataviz/graficos/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class GraficosConfig(AppConfig):
-    name = 'graficos'
