2be926a6ecc609208f5db95011bb46e718bb16f7
Renato Rocha Souza <rsouza.fgv@gmail.com>
2021-01-15 19:24:24 +0000

    Adding file to be imported"

---
+++ Notebooks/utils.py
@@ -0,0 +1,14 @@
+def square(n):
+    return n ** 2
+
+def cube(n):
+    return n ** 3
+
+my_constant = "3.14159"
+
+other_property = 3.14159
+
+print("This will be printed on import")
+
+if __name__ == '__main__':
+    print("This will not be printed in import")
