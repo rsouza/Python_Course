db6f9bdf5e3d6dc86d38e4349909d8042f78574a
Renato Rocha Souza <rsouza.fgv@gmail.com>
2022-06-24 23:57:28 +0200

    Adding code for importing

---
+++ Notebooks/Python_DataScience/histogram_multihist.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(19680801)
+
+n_bins = 10
+x = np.random.randn(1000, 3)
+
+fig, ((ax0, ax1), (ax2, ax3)) = plt.subplots(nrows=2, ncols=2)
+
+colors = ['red', 'tan', 'lime']
+ax0.hist(x, n_bins, density=True, histtype='bar', color=colors, label=colors)
+ax0.legend(prop={'size': 10})
+ax0.set_title('bars with legend')
+
+ax1.hist(x, n_bins, density=True, histtype='bar', stacked=True)
+ax1.set_title('stacked bar')
+
+ax2.hist(x, n_bins, histtype='step', stacked=True, fill=False)
+ax2.set_title('stack step (unfilled)')
+
+# Make a multiple-histogram of data-sets with different length.
+x_multi = [np.random.randn(n) for n in [10000, 5000, 2000]]
+ax3.hist(x_multi, n_bins, histtype='bar')
+ax3.set_title('different sample sizes')
+
+fig.tight_layout()
+plt.show()
